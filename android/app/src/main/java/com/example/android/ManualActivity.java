3bb23569a60a17c30e1ac67382c1241e0131d413
SvenHagenfors <gushagsv@student.gu.se>
2021-04-19 12:38:48 +0200

    I added Manual controller and all the buttons.

--- android/app/src/main/java/com/example/android/ManualActivity.java
+++ android/app/src/main/java/com/example/android/ManualActivity.java
@@ -14,12 +14,16 @@ public class ManualActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_manual);
 
         Button connect_manual = findViewById(R.id.connect_manual);
+        Button connect_automatic = findViewById(R.id.connect_automatic);
+        Button emergency_break = findViewById(R.id.emergency_break);
         Button forward_button = findViewById(R.id.forward_manual);
         Button backwards_button = findViewById(R.id.backwards_manual);
         Button left_button = findViewById(R.id.left_manual);
         Button right_button = findViewById(R.id.right_manual);
 
         connect_manual.setOnClickListener(this);
+        connect_automatic.setOnClickListener(this);
+        emergency_break.setOnClickListener(this);
         forward_button.setOnClickListener(this);
         backwards_button.setOnClickListener(this);
         left_button.setOnClickListener(this);
@@ -30,7 +34,11 @@ public class ManualActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.center_manual:
+            case R.id.connect_manual:
+                break;
+            case R.id.connect_automatic:
+                break;
+            case R.id.emergency_break:
                 break;
             case R.id.forward_manual:
                 break;
