ffdd5ab630f78ea76c78a35cc094be2bbae09d8d
guscuizi <81296052+guscuizi@users.noreply.github.com>
2021-05-14 00:12:09 +0800

    Add files via upload

--- android/app/src/main/java/com/example/android/ManualActivity.java
+++ android/app/src/main/java/com/example/android/ManualActivity.java
@@ -2,11 +2,12 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -31,15 +32,21 @@ public class ManualActivity extends AppCompatActivity implements View.OnClickLis
     private static final int STRAIGHT_ANGLE = 0;
     private static final int STEERING_ANGLE = 50;
     private static final int QOS = 1;
+    private static final int IMAGE_WIDTH = 320;
+    private static final int IMAGE_HEIGHT = 240;
 
     private MqttClient mMqttClient;
     private boolean isConnected = false;
+    private ImageView mCameraView;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
 
         mMqttClient = new MqttClient(getApplicationContext(), MQTT_SERVER, TAG);
+        mCameraView = findViewById(R.id.imageView);
+
         connectToMqttBroker();
 
         Button forward = findViewById(R.id.Forward);
@@ -56,6 +63,32 @@ public class ManualActivity extends AppCompatActivity implements View.OnClickLis
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        connectToMqttBroker();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mMqttClient.disconnect(new IMqttActionListener() {
+
+            @Override
+            public void onSuccess(IMqttToken asyncActionToken) {
+                Log.i(TAG, "Disconnected from broker");
+            }
+
+            @Override
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+            }
+        });
+    }
+
+
+
     private void connectToMqttBroker() {
         if (!isConnected) {
             mMqttClient.connect(TAG, "", new IMqttActionListener() {
@@ -68,6 +101,7 @@ public class ManualActivity extends AppCompatActivity implements View.OnClickLis
                     Toast.makeText(getApplicationContext(), successfulConnection, Toast.LENGTH_SHORT).show();
 
                     mMqttClient.subscribe("/smartcar/ultrasound/front", QOS, null);
+                    mMqttClient.subscribe("/smartcar/camera", QOS, null);
 
                 }
 
@@ -89,8 +123,23 @@ public class ManualActivity extends AppCompatActivity implements View.OnClickLis
 
                 @Override
                 public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    if (topic.equals("/smartcar/camera")) {
+                        final Bitmap bm = Bitmap.createBitmap(IMAGE_WIDTH, IMAGE_HEIGHT, Bitmap.Config.ARGB_8888);
 
-                    Log.i(TAG, "[MQTT] Topic: " + topic + " | Message: " + message.toString());
+                        final byte[] payload = message.getPayload();
+                        final int[] colors = new int[IMAGE_WIDTH * IMAGE_HEIGHT];
+                        for (int ci = 0; ci < colors.length; ++ci) {
+                            final byte r = payload[3 * ci];
+                            final byte g = payload[3 * ci + 1];
+                            final byte b = payload[3 * ci + 2];
+                            colors[ci] = Color.rgb(r, g, b);
+                        }
+                        bm.setPixels(colors, 0, IMAGE_WIDTH, 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
+
+                        mCameraView.setImageBitmap(bm);
+                    } else {
+                        Log.i(TAG, "[MQTT] Topic: " + topic + " | Message: " + message.toString());
+                    }
                 }
 
                 @Override
@@ -101,6 +150,7 @@ public class ManualActivity extends AppCompatActivity implements View.OnClickLis
         }
 
     }
+
     void drive(int throttleSpeed, int steeringAngle, String actionDescription) {
         if (!isConnected) {
             final String notConnected = "Not connected (yet)";
@@ -153,4 +203,6 @@ public class ManualActivity extends AppCompatActivity implements View.OnClickLis
                 break;
         }
     }
+
+
 }
