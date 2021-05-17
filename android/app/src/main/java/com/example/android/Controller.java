d731c7e2abc80c53165b6ffedbdf4b85fd67fdc6
leilab <leilab@net.chalmers.se>
2021-05-17 23:36:23 +0200

    Implement Delivery for tracebility

--- android/app/src/main/java/com/example/android/Controller.java
+++ android/app/src/main/java/com/example/android/Controller.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Controller {
 
     public ArrayList<User> userList= new ArrayList<User>();
+    public ArrayList<Delivery> deliveries= new ArrayList<Delivery>();
 
     public Controller(ArrayList<User> userList) {
 
@@ -23,20 +24,26 @@ public class Controller {
         userList.add(receiver);
     }
     // delivery: ID confirmation(yes:no)
+    public void createDelivery(Mailman mailman,Receiver receiver){
+        Delivery delivery=new Delivery(mailman.getID(),mailman.getUserName(),receiver.getID(),receiver.getUserName());
+        deliveries.add(delivery);
+    }
 
     public void expectDelivery(Mailman mailman, Receiver receiver){
         String content="You have a mail ready to be delivered to you, please wait for it it's on it's way to you! Vroom vroom !";
         Message message=new Message(content,mailman.getUserName(),receiver.getUserName());
         receiver.getNotification().add(message);
     }
-    public void pickupMessage(Mailman mailman, Receiver receiver){
+    public void pickupMessage(Mailman mailman, Receiver receiver,Delivery delivery){
         String content="Your mail has arrived and waits for you ! please confirm pickup :) ";
         Message message=new Message(content,mailman.getUserName(),receiver.getUserName());
         receiver.getNotification().add(message);
+        delivery.setStatus("Delivered");
     }
 
-    public void ConfirmPickupMessage(Mailman mailman, Receiver receiver){
-        String content="Confirmed!";
+    public void ConfirmPickupMessage(Mailman mailman, Receiver receiver,Delivery delivery){
+        String content="Pickup Confirmed!";
+        delivery.setStatus("Picked-up");
         Message message=new Message(content,mailman.getUserName(),receiver.getUserName());
         mailman.getNotification().add(message);
     }
--- android/app/src/main/java/com/example/android/Delivery.java
+++ android/app/src/main/java/com/example/android/Delivery.java
@@ -1,4 +1,65 @@
 package com.example.android;
 
+import java.util.UUID;
+
 public class Delivery {
+    private String ID;
+    private String mailmanID;
+    private String mailmanName;
+    private String receiverID;
+    private String receiverName;
+    private String status;
+
+
+    public Delivery(String mailmanID, String mailmanName, String receiverID, String receiverName) {
+        UUID uuid = UUID.randomUUID();
+        this.ID = uuid.toString();
+        this.ID = ID.substring(0, Math.min(ID.length(), 3));
+
+        this.mailmanID = mailmanID;
+        this.mailmanName = mailmanName;
+        this.receiverID = receiverID;
+        this.receiverName = receiverName;
+        this.status = "Not delivered";
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getMailmanID() {
+        return mailmanID;
+    }
+
+    public String getMailmanName() {
+        return mailmanName;
+    }
+
+    public String getReceiverID() {
+        return receiverID;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Delivery{" +
+                "ID='" + ID + '\'' +
+                ", mailmanID='" + mailmanID + '\'' +
+                ", mailmanName='" + mailmanName + '\'' +
+                ", receiverID='" + receiverID + '\'' +
+                ", receiverName='" + receiverName + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
