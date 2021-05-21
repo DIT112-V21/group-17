51b73fd5a4012ee70e4d91f1529267c020d61803
leilab <leilab@net.chalmers.se>
2021-05-21 21:17:57 +0200

    Fix list of receivers to be created only once the app is started to avoid redandency in the list

--- android/app/src/main/java/com/example/android/ReceiverListActivity.java
+++ android/app/src/main/java/com/example/android/ReceiverListActivity.java
@@ -17,25 +17,6 @@ public class ReceiverListActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: Started.");
         ListView mListView = (ListView) findViewById(R.id.listView);
 
-        //Create the Person objects
-        Controller.addReceiver("Leila","123"," room 1");
-        Controller.addReceiver("Sven","123"," room 2");
-        Controller.addReceiver("Maab","123"," room 3");
-        Controller.addReceiver("Sofia","123"," room 4");
-        Controller.addReceiver("Elif","123"," room 5");
-        Controller.addReceiver("Cui","123"," room 6");
-        Controller.addReceiver("Receiver_1","123"," room 7");
-        Controller.addReceiver("Receiver_1","123"," room 8");
-        Controller.addReceiver("Receiver_1","123"," room 9");
-        Controller.addReceiver("Receiver_1","123"," room 10");
-        Controller.addReceiver("Receiver_1","123"," room 11");
-        Controller.addReceiver("Receiver_1","123"," room 12");
-        Controller.addReceiver("Receiver_1","123"," room 13");
-        Controller.addReceiver("Receiver_1","123"," room 14");
-        Controller.addReceiver("Receiver_1","123"," room 15");
-        Controller.addReceiver("Receiver_1","123"," room 16");
-        Controller.addReceiver("Receiver_1","123"," room 17");
-        Controller.addReceiver("Receiver_1","123"," room 18");
 
         ReceiverListAdapter adapter = new ReceiverListAdapter(this, R.layout.adapter_receiver_list_view_layout, Controller.receiversList);
         mListView.setAdapter(adapter);
