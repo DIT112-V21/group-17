435162041ed0f78529a9b4b00fab632b1d7346be
guscuizi <81296052+guscuizi@users.noreply.github.com>
2021-07-30 22:50:43 +0800

    Add mailman's other activity

---
+++ android/app/src/main/java/com/example/android/MailmanMessageListActivity.java
@@ -0,0 +1,43 @@
+package smart.delirover.app;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.delirover.app.R;
+
+public class MailmanMessageListActivity extends AppCompatActivity {
+
+    private static final String TAG = "MailmanMessageListActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mailman_message_list);
+
+        Log.d(TAG, "onCreate: Started.");
+        ListView mListView = (ListView) findViewById(R.id.listViewMailmanMessageList);
+
+
+        MailmanMessageListAdapter adapter = new MailmanMessageListAdapter(this, R.layout.adapter_mailman_message_list_view_layout, Controller.mailmanMessageList());
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View tvMessage, int position, long id) {
+                openDialog(position);
+
+            }
+        });
+    }
+    public void openDialog(int position){
+        String messageContent=Controller.mailmanMessageList().get(position).toString();
+        String messageTitle=Controller.mailmanMessageList().get(position).getTitle();
+        MessageDialog messageD = new MessageDialog(messageTitle,messageContent);
+        messageD.show(getSupportFragmentManager(), "Message dialog");
+    }
+}
---
+++ android/app/src/main/java/com/example/android/MailmanMessageListAdapter.java
@@ -0,0 +1,80 @@
+package smart.delirover.app;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.delirover.app.R;
+
+import java.util.ArrayList;
+
+public class MailmanMessageListAdapter extends ArrayAdapter<Message> {
+
+    private static final String TAG = "MailmanMessageListAdapter";
+    private static int position;
+
+    private Context mContext;
+    private int mResource;
+
+
+    public MailmanMessageListAdapter(Context context, int resource, ArrayList<Message> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mResource = resource;
+    }
+
+    ArrayList<Message> messages=Controller.mailmanMessageList();
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Log.w(TAG,"message view started" );
+
+        Message message=messages.get(position);
+        String name = getItem(position).getSenderName();
+        String senderID = message.getSenderID();
+        String mailmanID=Controller.getLoggedInMailmanID();
+
+
+        LayoutInflater inflater=LayoutInflater.from(mContext);
+        convertView=inflater.inflate(mResource,parent,false);
+        TextView tvMessage = (TextView) convertView.findViewById(R.id.tv_mailman_message);
+        Button pickup = (Button) convertView.findViewById(R.id.pickup_button);
+        String message_title=message.getSenderName()+" "+message.getTitle();
+
+        tvMessage.setText(message_title);
+
+        if(message.getTitle().equals("Available")) {
+            pickup.setVisibility(Button.VISIBLE);
+            if(!message.getMessageStatus().equals("sent")) {
+                pickup.setEnabled(true);
+
+
+                pickup.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final String pickup_sent = "pick-up delivery message sent to " + name;
+                        Controller.sendPickupMessage(mailmanID, senderID);
+
+                        pickup.setEnabled(false);
+                        message.setMessageStatus("sent");
+
+                        Log.w(TAG, pickup_sent);
+                        Toast.makeText(mContext.getApplicationContext(), pickup_sent, Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        }
+
+
+
+        return convertView;
+    }
+}
